063951a5fd760d7e93fcaa35d5ca729b7cd5d403
Mark <mark@example.dev>
2021-09-30 00:59:36 -0700

    POST and PUT routes for invoices

--- routes/invoices.js
+++ routes/invoices.js
@@ -3,45 +3,34 @@ const router = new express.Router();
 const db = require('../db');
 const ExpressError = require('../expressError');
 
-// function validatePOST(req, res, next){
-//     const json = req.body;
-//     const errors = [];
-//     // Field checking
-//     if (!json.code) errors.push('code ');
-//     if (!json.name) errors.push('name ');
-//     if (!json.description) errors.push('description ');
-//     if (errors.length > 0){
-//         return next(new ExpressError(`Missing fields: ${errors}`, 400));
-//     }
-//     // Type checking
-//     if (typeof json.code !== 'string') errors.push("'code' field must be of type string");
-//     if (typeof json.name !== 'string') errors.push("'name' field must be of type string");
-//     if (typeof json.description !== 'string') errors.push("'description' field must be of type string");
-//     if (errors.length > 0){
-//         return next(new ExpressError(`Type errors: ${errors}`, 400));
-//     }
+function validatePOST(req, res, next){
+    const json = req.body;
+    const errors = [];
+    // Field checking
+    if (!json.comp_code) errors.push('comp_code ');
+    if (!json.amt) errors.push('amt ');
+    if (errors.length > 0){
+        return next(new ExpressError(`Missing fields: ${errors}`, 400));
+    }
+    // Type checking
+    if (typeof json.comp_code !== 'string') errors.push("'comp_code' field must be of type string");
+    if (typeof json.amt !== 'number') errors.push("'amt' field must be of type number");
+    if (errors.length > 0){
+        return next(new ExpressError(`Type errors: ${errors}`, 400));
+    }
 
-//     return next();
-// }
+    return next();
+}
 
-// function validatePUT(req, res, next){
-//     const json = req.body;
-//     const errors = [];
-//     // Field checking
-//     if (!json.name) errors.push('name ');
-//     if (!json.description) errors.push('description ');
-//     if (errors.length > 0){
-//         return next(new ExpressError(`Missing fields: ${errors}`, 400));
-//     }
-//     // Type checking
-//     if (typeof json.name !== 'string') errors.push("'name' field must be of type string");
-//     if (typeof json.description !== 'string') errors.push("'description' field must be of type string");
-//     if (errors.length > 0){
-//         return next(new ExpressError(`Type errors: ${errors}`, 400));
-//     }
+function validatePUT(req, res, next){
+    const json = req.body;
+    // Field checking
+    if (!json.amt) return next(new ExpressError(`Missing field: amt`, 400));
+    // Type checking
+    if (typeof json.amt !== 'number') return next(new ExpressError(`Type error: amt must be of type number`, 400));
 
-//     return next();
-// }
+    return next();
+}
 
 router.get('/', async (req, res, next) => {
     try{
@@ -68,32 +57,34 @@ router.get('/:id', async (req, res, next) => {
     }
 });
 
-// router.post('/', validatePOST, async (req, res, next) => {
-//     try{
-//         const {code, name, description} = req.body;
-//         const result = await db.query(
-//             'INSERT INTO companies (code, name, description) VALUES ($1, $2, $3) RETURNING code, name, description',
-//             [code, name, description]
-//         );
-//         return res.status(201).json({company: result.rows[0]});
-//     } catch (err) {
-//         return next(err);
-//     }
-// });
+router.post('/', validatePOST, async (req, res, next) => {
+    try{
+        const {comp_code, amt} = req.body;
+        const result = await db.query(
+            `INSERT INTO invoices (comp_code, amt) VALUES ($1, $2)
+            RETURNING id, comp_code, amt, paid, add_date, paid_date`,
+            [comp_code, amt]
+        );
+        return res.status(201).json({invoice: result.rows[0]});
+    } catch (err) {
+        return next(err);
+    }
+});
 
-// router.put('/:code', validatePUT, async (req, res, next) => {
-//     try{
-//         const {name, description} = req.body;
-//         const result = await db.query(
-//             'UPDATE companies SET name = $1, description = $2 WHERE code = $3 RETURNING code, name, description',
-//             [name, description, req.params.code]
-//         );
-//         if (result.rows[0]) return res.json({company: result.rows[0]});
-//         else return next(); // Continue to 404 handler
-//     } catch (err) {
-//         return next(err);
-//     }
-// });
+router.put('/:id', validatePUT, async (req, res, next) => {
+    try{
+        const { amt } = req.body;
+        const result = await db.query(
+            `UPDATE invoices SET amt = $1 WHERE id = $2
+            RETURNING id, comp_code, amt, paid, add_date, paid_date`,
+            [amt, req.params.id]
+        );
+        if (result.rows[0]) return res.json({invoice: result.rows[0]});
+        else return next(); // Continue to 404 handler
+    } catch (err) {
+        return next(err);
+    }
+});
 
 // router.delete('/:code', async (req, res, next) => {
 //     try{
