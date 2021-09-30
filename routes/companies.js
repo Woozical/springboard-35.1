ac415d3ebfc4d3d062437d5257438a6aa3a9ce62
Mark <mark@example.dev>
2021-09-30 00:02:49 -0700

    POST and PUT routes for companies

--- routes/companies.js
+++ routes/companies.js
@@ -3,15 +3,45 @@ const router = new express.Router();
 const db = require('../db');
 const ExpressError = require('../expressError');
 
-// router.get('/', (req, res, next) => {
-//     db.query('SELECT * FROM companies')
-//     .then( (results) => {
-//         return res.json({companies: results.rows});
-//     })
-//     .catch( (err) => {
-//         return next(err)
-//     });
-// });
+function validatePOST(req, res, next){
+    const json = req.body;
+    const errors = [];
+    // Field checking
+    if (!json.code) errors.push('code ');
+    if (!json.name) errors.push('name ');
+    if (!json.description) errors.push('description ');
+    if (errors.length > 0){
+        return next(new ExpressError(`Missing fields: ${errors}`, 400));
+    }
+    // Type checking
+    if (typeof json.code !== 'string') errors.push("'code' field must be of type string");
+    if (typeof json.name !== 'string') errors.push("'name' field must be of type string");
+    if (typeof json.description !== 'string') errors.push("'description' field must be of type string");
+    if (errors.length > 0){
+        return next(new ExpressError(`Type errors: ${errors}`, 400));
+    }
+
+    return next();
+}
+
+function validatePUT(req, res, next){
+    const json = req.body;
+    const errors = [];
+    // Field checking
+    if (!json.name) errors.push('name ');
+    if (!json.description) errors.push('description ');
+    if (errors.length > 0){
+        return next(new ExpressError(`Missing fields: ${errors}`, 400));
+    }
+    // Type checking
+    if (typeof json.name !== 'string') errors.push("'name' field must be of type string");
+    if (typeof json.description !== 'string') errors.push("'description' field must be of type string");
+    if (errors.length > 0){
+        return next(new ExpressError(`Type errors: ${errors}`, 400));
+    }
+
+    return next();
+}
 
 router.get('/', async (req, res, next) => {
     try{
@@ -25,13 +55,38 @@ router.get('/', async (req, res, next) => {
 router.get('/:code', async (req, res, next) => {
     try{
         const result = await db.query('SELECT * FROM companies WHERE code = $1', [req.params.code]);
-        if (result.rows[0]) return res.json(result.rows[0])
+        if (result.rows[0]) return res.json({company: result.rows[0]});
         else return next() // Continue to 404 handler
     } catch (err) {
         return next(err);
     }
 });
 
+router.post('/', validatePOST, async (req, res, next) => {
+    try{
+        const {code, name, description} = req.body;
+        const result = await db.query(
+            'INSERT INTO companies (code, name, description) VALUES ($1, $2, $3) RETURNING code, name, description',
+            [code, name, description]
+        );
+        return res.json({company: result.rows[0]});
+    } catch (err) {
+        return next(err);
+    }
+});
 
+router.put('/:code', validatePUT, async (req, res, next) => {
+    try{
+        const {name, description} = req.body;
+        const result = await db.query(
+            'UPDATE companies SET name = $1, description = $2 WHERE code = $3 RETURNING code, name, description',
+            [name, description, req.params.code]
+        );
+        if (result.rows[0]) return res.json({company: result.rows[0]});
+        else return next() // Continue to 404 handler
+    } catch (err) {
+        return next(err);
+    }
+});
 
 module.exports = router;
